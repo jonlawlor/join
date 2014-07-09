1fb5e368498e18722590efb54d7662bd9bc45f5f
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2014-07-09 00:27:58 -0400

    Get merge join working After implementing some tests (and some negative results, like having my computer hang) I added in some testing apparatus to determine program flow.  There were several areas that could either result in infinite loops, invalid slice references, or incorrect output, but eventually I got them under control.  It passes now, woo hoo!

--- mergejoin.go
+++ mergejoin.go
@@ -2,6 +2,7 @@
 
 package join
 
+
 // TupComp represents 3 valued comparison between two tuples.  It can either be
 // LT (which means that the left tuple is less than the right), EQ (which means
 // that they are equal), or GT (which means that the left tuple is greater than
@@ -21,20 +22,25 @@ const (
 
 // MergeJoiner is the interface for merge join in memory
 type MergeJoiner interface {
-	MergeJoin(lSize, rSize chan int, done chan struct{}) (compare func(i, j int) TupComp, combine func(i, j int))
+	MergeJoin(lSize, rSize chan int, updateLeft, updateRight, done chan struct{}) (compare func(i, j int) TupComp, combine func(i, j int))
 }
 
 // Join is an implementation of merge join
 func Join(e MergeJoiner) {
-  // Construct the block size channels.  They are unbuffered, so that the block
+	// Construct the block size channels.  They are unbuffered, so that the block
 	// producer can determine when join is finished reading from the blocks.
 	lSize := make(chan int, 0)
 	rSize := make(chan int, 0)
+	updateLeft := make(chan struct{}, 0)
+	updateRight := make(chan struct{}, 0)
 
 	done := make(chan struct{}, 0) // signal to terminate execution early
-	compare, combine := e.MergeJoin(lSize, rSize, done)
+	compare, combine := e.MergeJoin(lSize, rSize, updateLeft, updateRight, done)
 
 	// start reading from the blocks
+
+	updateLeft <- struct{}{}
+	updateRight <- struct{}{}
 	lSz, ok := <-lSize
 	if !ok {
 		close(done)
@@ -45,22 +51,29 @@ func Join(e MergeJoiner) {
 		close(done)
 		return
 	}
-
 	for {
 		switch compare(0, 0) {
 		case LT:
+			updateLeft <- struct{}{}
 			lSz, ok = <-lSize
 			if !ok {
+				// close the other sync channels
+				close(updateRight)
 				close(done)
 				return
 			}
 		case GT:
+
+			updateRight <- struct{}{}
 			rSz, ok = <-rSize
 			if !ok {
+				// close the other sync channels
+				close(updateLeft)
 				close(done)
 				return
 			}
 		case EQ:
+
 			// we've already compared 0,0, so combine it
 			combine(0, 0)
 
@@ -72,6 +85,15 @@ func Join(e MergeJoiner) {
 				for j := 0; j < rSz; j++ {
 					combine(i, j)
 				}
+			}
+			// now advance the left relation
+			updateLeft <- struct{}{}
+			lSz, ok = <-lSize
+			if !ok {
+				// close the other sync channels
+				close(updateRight)
+				close(done)
+				return
 			}
 		}
 	}
--- mergejoin_mem_test.go
+++ mergejoin_mem_test.go
@@ -1,62 +1,193 @@
 // mergejoin_mem_test contains tests for merge join when the data structures are
 // all held in memory.
 
+package join
+
+import (
+	"testing"
+)
+
+// natural join foobar[foo, bar] with barbaz[bar, baz] resulting in
+// foobarbaz[foo, bar, baz]
 
 type foobar struct {
-  foo int
-  bar int
+	foo int
+	bar int
 }
 
 type barbaz struct {
-  bar int
-  baz int
+	bar int
+	baz int
 }
 
 type foobarbaz struct {
-  foo int
-  bar int
-  baz int
+	foo int
+	bar int
+	baz int
 }
 
 type joinExpr struct {
-  left  []foobar
-  right []barbaz
-  res   []foobarbaz
+	left  []foobar
+	right []barbaz
+	res   []foobarbaz
 }
 
-func (e *joinExpr) LeftLen() int  { return len(e.left) }
-func (e *joinExpr) RightLen() int { return len(e.right) }
+// test implementation of MergeJoin for the foo,bar * bar, baz -> foo, bar, baz
+// join.
+func (e *joinExpr) MergeJoin(lSize, rSize chan int, updateLeft, updateRight, done chan struct{}) (compare func(i, j int) TupComp, combine func(i, j int)) {
+	if len(e.left) == 0 || len(e.right) == 0 {
+		// zero join
+		close(lSize)
+		close(rSize)
+		return
+	}
 
-func (e *joinExpr) LeftEQ(i, j int) bool  { return e.left[i].bar == e.left[j].bar }
-func (e *joinExpr) RightEQ(i, j int) bool { return e.right[i].bar == e.right[j].bar }
-func (e *joinExpr) LRLess(i, j int) bool  { return e.left[i].bar < e.right[j].bar }
-func (e *joinExpr) LREqual(i, j int) bool { return e.left[i].bar == e.right[j].bar }
+	var (
+		leftBlock  []foobar
+		rightBlock []barbaz
+	)
+	// start the left block reader
+	go func() {
 
-func (e *joinExpr) Combine(i, j int) {
-  e.res = append(e.res, foobarbaz{e.left[i].foo, e.left[i].bar, e.right[i].baz})
+		sz := 1
+		start := 0
+
+		for i := 1; i < len(e.left); i++ {
+			if sz == 0 || e.left[i].bar == e.left[start].bar {
+				sz++
+				continue
+			}
+
+			// wait to be told to update the slices
+
+			<-updateLeft
+			// update the left slice
+			leftBlock = e.left[start:i]
+			// send the new size and then reset
+			select {
+			case lSize <- sz:
+				sz = 1
+				start = i
+			case <-done:
+				// early cancellation
+				return
+			}
+		}
+
+		// send the last block
+
+		// wait to be told to update the slices
+		<-updateLeft
+		// update the left slice
+		leftBlock = e.left[start:]
+		// send the new size and then reset
+		select {
+		case lSize <- sz:
+		case <-done:
+			// early cancellation
+			return
+		}
+		<-updateLeft
+		close(lSize)
+	}()
+
+	// start the right block reader
+	go func() {
+
+		sz := 1
+		start := 0
+
+		for i := 1; i < len(e.right); i++ {
+			if sz == 0 || e.right[i].bar == e.right[start].bar {
+				sz++
+				continue
+			}
+
+			// wait to be told to update the slices
+			<-updateRight
+			// update the left slice
+			rightBlock = e.right[start:i]
+			// send the new size and then reset
+
+			select {
+			case rSize <- sz:
+				sz = 1
+				start = i
+			case <-done:
+				// early cancellation
+				return
+			}
+		}
+
+		// send the last block
+		// wait to be told to update the slices
+		<-updateRight
+		// update the left slice
+		rightBlock = e.right[start:]
+		// send the new size and then reset
+		select {
+		case rSize <- sz:
+		case <-done:
+			// early cancellation
+			return
+		}
+		<-updateRight
+		close(rSize)
+	}()
+
+	compare = func(i, j int) TupComp {
+
+		if leftBlock[i].bar == rightBlock[j].bar {
+			return EQ
+		}
+		if leftBlock[i].bar < rightBlock[j].bar {
+			return LT
+		}
+		return GT
+	}
+	combine = func(i, j int) {
+		e.res = append(e.res, foobarbaz{leftBlock[i].foo, leftBlock[i].bar, rightBlock[j].baz})
+	}
+	return
 }
 
-func main() {
+func TestMemMergeJoin(t *testing.T) {
 
-  l := []foobar{
-    {1, 1},
-    {1, 2},
-    {2, 3},
-    {3, 3},
-    {1, 4},
-    {2, 5},
-  }
+	l := []foobar{
+		{1, 1},
+		{1, 2},
+		{2, 3},
+		{3, 3},
+		{1, 4},
+		{2, 5},
+	}
 
-  r := []barbaz{
-    {1, 1},
-    {1, 2},
-    {2, 2},
-    {3, 1},
-    {3, 4},
-    {4, 4},
-  }
-
-  j := &joinExpr{left: l, right: r, res: []foobarbaz{}}
-
-  fmt.Println("Hello, playground", j)
+	r := []barbaz{
+		{1, 1},
+		{1, 2},
+		{2, 2},
+		{3, 1},
+		{3, 4},
+		{4, 4},
+	}
+	e := &joinExpr{left: l, right: r, res: []foobarbaz{}}
+	Join(e)
+	expectTable := []foobarbaz{
+		{1, 1, 1},
+		{1, 1, 2},
+		{1, 2, 2},
+		{2, 3, 1},
+		{2, 3, 4},
+		{3, 3, 1},
+		{3, 3, 4},
+		{1, 4, 4},
+	}
+	if resLen := len(e.res); resLen != len(expectTable) {
+		t.Errorf("Join() results have len => %v, want %v", e.res, len(expectTable))
+	}
+	for i := range expectTable {
+		if e.res[i] != expectTable[i] {
+			t.Errorf("Join() result %d tuple => %v, want %v", i, e.res[i], expectTable[i])
+		}
+	}
 }
