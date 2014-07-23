f7b33ad649f020ec8642739d8062bfce6420d8f0
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2014-07-22 22:57:43 -0400

    re-implement group, and add parallel version

--- group_test.go
+++ group_test.go
@@ -1,10 +1,10 @@
 package relpipes
 
 import (
-	"runtime"
+	"sync"
 	"sort"
 	"testing"
-	"sync"
+	"runtime"
 	)
 // a set of tests and benchmarks for group
 
@@ -163,88 +163,65 @@ func BenchmarkGroupOrdered_1000000_100000_1(b *testing.B) {
 
 // groupUnordered takes two channels, one for input and one for
 // results, and passes tuples that belong to the same group
-// to the appropriate output channel. It also takes n, which is
-// the buffer size that should be used for the inner result channels.
-func groupUnordered(in <-chan fooBar, res chan (chan fooBar), n int) {
-
+// to the appropriate output channel.
+func groupUnordered(in <-chan fooBar, res chan (chan fooBar)) {
 	go func() {
-		m := make(map[subFooBar]chan fooBar)
-		var wg sync.WaitGroup
+		// recv the first value from the input, which will determine
+		// the first group that we'll send.  If it is already closed
+		// then we can close the result.
+		v, ok := <- in
+		if !ok {
+			close(res)
+			return
+		}
 
-		// read each input,
-		for v := range in {
-			// if it is has not already been
-			// encountered, create a new goroutine
-			// to handle it
-			sub := proj(&v)
-			if _, exist := m[sub]; !exist {
-				wg.Add(1)
-				g := make(chan fooBar)
-				m[sub] = g
-				go makeGroup(g, res, &wg, n)
+		// figure out the subdomain for the first (and only active)
+		// group.
+		groupSub := proj(&v)
+
+		// send the first group and tuple
+		g := make(chan fooBar,1)
+		res <- g
+		g <- v
+
+		// map containing the inactive groups
+		mem := make(map[subFooBar][]fooBar)
+
+		for v = range in {
+			newSub := proj(&v)
+			if newSub == groupSub {
+				// it belongs to the active group
+				g <- v
+				continue
 			}
-			m[sub] <- v
+			// otherwise it is a new tuple that has to be saved
+			if sl, exists := mem[newSub]; !exists {
+				// we've encountered a new group, so initialize
+				// the map's value
+				mem[newSub] = []fooBar{v}
+			} else {
+				mem[newSub] = append(sl, v)
+			}
 		}
-		// close the grouping channels
-		for _, ch := range m {
-			close(ch)
+		// once we've read all of the tuples, we have to go through
+		// the memory and send the results
+		close(g)
+		for _, sl := range mem {
+			g = make(chan fooBar,1)
+			res <- g
+			for _, v = range sl {
+				g <- v
+			}
+			close(g)
 		}
-		// once the groups are all finished,
-		// close the result group channel
-		wg.Wait()
 		close(res)
 	}()
 	return
 }
 
-// makeGroup handles an individual group.  It will never block on
-// a recv from groupChan
-func makeGroup(in chan fooBar, res chan (chan fooBar), wg *sync.WaitGroup, n int) {
-
-	groupChan := make(chan fooBar, n)
-	sendChan := groupChan
-	sentTup := true
-	tupBuf := make([]fooBar, 0) // a stack of tuples
-	var v fooBar
-
-	for {
-		if bufLen := len(tupBuf); sentTup && bufLen > 0 {
-			// there is a need to send a new value
-			// pop one of the tuples out of the buffer
-			v, tupBuf = tupBuf[bufLen-1], tupBuf[:bufLen-1]
-			sentTup = false
-			sendChan = groupChan
-		} else {
-			if sentTup {
-				if in == nil {
-					// there is nothing left to send
-					break
-				}
-				sendChan = nil
-			}
-		}
-		select {
-		case res <- groupChan:
-			res = nil
-		case sendChan <- v:
-			sentTup = true
-		case tup, ok := <-in:
-			if !ok {
-				// input channel is closed
-				in = nil
-				break
-			}
-			tupBuf = append(tupBuf, tup)
-		}
-	}
-	wg.Done()
-	close(groupChan)
-}
-
 // benchmark
 
 func benchGroupUnordered(b *testing.B, tupN, fooN, barN int) {
-	mc := runtime.GOMAXPROCS(2)
 	in := makeFooBar(tupN, fooN, barN)
 	b.StopTimer()
 	b.ResetTimer()
@@ -252,13 +229,11 @@ func benchGroupUnordered(b *testing.B, tupN, fooN, barN int) {
 		res := make(chan (chan fooBar), 1)
 
 		b.StartTimer()
-		groupUnordered(fooBarChan(in, 1), res, 2)
+		groupUnordered(fooBarChan(in, 1), res)
 
 		emptyFooBarGroup(res)
 		b.StopTimer()
 	}
-	runtime.GOMAXPROCS(mc)
-
 }
 
 // benchmarks for group with cardinality ~3
@@ -319,4 +294,170 @@ func BenchmarkGroupUnordered_100000_100000_1(b *testing.B) {
 }
 func BenchmarkGroupUnordered_1000000_100000_1(b *testing.B) {
 	benchGroupUnordered(b, 1000000, 100000, 1)
+}
+
+// groupUnordered takes two channels, one for input and one for
+// results, and passes tuples that belong to the same group
+// to the appropriate output channel.
+func groupUnorderedParallel(in <-chan fooBar, res chan (chan fooBar), n int) {
+	// We split the input into n goroutines by performing
+	// a modulo on the input tuples, and using that to
+	// determine which stripe to send the tuple to.
+	stripes := make([]chan fooBar, n)
+
+	// We have to wait for all goroutines to finish before
+	// closing the results channel.
+	var wg sync.WaitGroup
+	wg.Add(n)
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+
+	for i := 0; i < n; i++ {
+		stripes[i] = make(chan fooBar, 1)
+		go func(stripe <-chan fooBar) {
+			// recv the first value from the input, which will determine
+			// the first group that we'll send.  If it is already closed
+			// then we can close the result.
+			v, ok := <- in
+			if !ok {
+				wg.Done()
+				return
+			}
+
+			// figure out the subdomain for the first (and only active)
+			// group.
+			groupSub := proj(&v)
+
+			// send the first group and tuple
+			g := make(chan fooBar,1)
+			res <- g
+			g <- v
+
+			// map containing the inactive groups
+			mem := make(map[subFooBar][]fooBar)
+
+			for v = range in {
+				newSub := proj(&v)
+				if newSub == groupSub {
+					// it belongs to the active group
+					g <- v
+					continue
+				}
+				// otherwise it is a new tuple that has to be saved
+				if sl, exists := mem[newSub]; !exists {
+					// we've encountered a new group, so initialize
+					// the map's value
+					mem[newSub] = []fooBar{v}
+					} else {
+						mem[newSub] = append(sl, v)
+					}
+				}
+				// once we've read all of the tuples, we have to go through
+				// the memory and send the results
+				close(g)
+				for _, sl := range mem {
+					g = make(chan fooBar,1)
+					res <- g
+					for _, v = range sl {
+						g <- v
+					}
+					close(g)
+				}
+				wg.Done()
+			}(stripes[i])
+		}
+
+	// use modulo to send inputs to the appropriate
+	// channel
+	go func() {
+		for v := range in {
+			stripes[v.foo%n] <- v
+		}
+		// close intermediate channels
+		for i := 0; i < n; i++ {
+			close(stripes[i])
+		}
+	}()
+	return
+}
+
+// benchmarks
+
+func benchGroupUnorderedParallel(b *testing.B, tupN, fooN, barN int) {
+	mc := runtime.GOMAXPROCS(2)
+	in := makeFooBar(tupN, fooN, barN)
+	b.StopTimer()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		res := make(chan (chan fooBar), 2)
+
+		b.StartTimer()
+		groupUnorderedParallel(fooBarChan(in, 1), res, 2)
+
+		emptyFooBarGroup(res)
+		b.StopTimer()
+	}
+	runtime.GOMAXPROCS(mc)
+}
+
+// benchmarks for group with cardinality ~3
+func BenchmarkGroupUnorderedParallel_10_3_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 10, 3, 1)
+}
+func BenchmarkGroupUnorderedParallel_100_3_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 100, 3, 1)
+}
+func BenchmarkGroupUnorderedParallel_1000_3_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 1000, 3, 1)
+}
+func BenchmarkGroupUnorderedParallel_10000_3_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 10000, 3, 1)
+}
+func BenchmarkGroupUnorderedParallel_100000_3_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 100000, 3, 1)
+}
+func BenchmarkGroupUnorderedParallel_1000000_3_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 1000000, 3, 1)
+}
+
+// benchmarks for group with cardinality ~1000
+func BenchmarkGroupUnorderedParallel_10_1000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 10, 1000, 1)
+}
+func BenchmarkGroupUnorderedParallel_100_1000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 100, 1000, 1)
+}
+func BenchmarkGroupUnorderedParallel_1000_1000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 1000, 1000, 1)
+}
+func BenchmarkGroupUnorderedParallel_10000_1000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 10000, 1000, 1)
+}
+func BenchmarkGroupUnorderedParallel_100000_1000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 100000, 1000, 1)
+}
+func BenchmarkGroupUnorderedParallel_1000000_1000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 1000000, 1000, 1)
+}
+
+// benchmarks for group with cardinality ~100000
+func BenchmarkGroupUnorderedParallel_10_100000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 10, 100000, 1)
+}
+func BenchmarkGroupUnorderedParallel_100_100000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 100, 100000, 1)
+}
+func BenchmarkGroupUnorderedParallel_1000_100000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 1000, 100000, 1)
+}
+func BenchmarkGroupUnorderedParallel_10000_100000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 10000, 100000, 1)
+}
+func BenchmarkGroupUnorderedParallel_100000_100000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 100000, 100000, 1)
+}
+func BenchmarkGroupUnorderedParallel_1000000_100000_1(b *testing.B) {
+	benchGroupUnorderedParallel(b, 1000000, 100000, 1)
 }
